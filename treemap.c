e8cfa02114096ff4c6f510edce393b939e262ee2
Replit user <>
2023-04-22 20:21:47 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -179,7 +179,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
     while(tree -> current != NULL){
       
       x= nextTreeMap(tree);
-      if(is_equal(tree, key, x-> key)) return x;
+      if(is_equal(tree, x-> key,key )) return x;
     }
   }
   return NULL;
