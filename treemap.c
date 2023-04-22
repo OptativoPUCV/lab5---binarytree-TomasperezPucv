e23eb48e8af1f4008b683c4227608c384cf6462b
Replit user <>
2023-04-22 23:17:26 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -203,10 +203,10 @@ Pair * upperBound(TreeMap * tree, void* key) {
   
   
   Pair *x = searchTreeMap(tree,key);
-  if(is_equal(tree, key, x-> key)) return x;
+  if(is_equal(tree, key, x-> key) return x;
   else{
     
-    while(tree -> current != NULL){
+    /*while(tree -> current != NULL){
       if(tree -> lower_than(tree -> current -> pair -> key, key)) return tree -> current -> pair;
       x= nextTreeMap(tree);
     }
@@ -219,7 +219,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
       
       x= nextTreeMap(tree);
       if(is_equal(tree, x-> key,key )) return x;
-    }
+    }*/
   }
   return NULL;
 }
