23ac139e4daf3061c2774a629b525ee3591b177c
Replit user <>
2023-04-22 16:45:38 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -200,16 +200,16 @@ Pair * nextTreeMap(TreeMap * tree) {
     next = minimum(tree -> current -> right);
     return next -> pair;
   }
-
+  
   
   //next = next -> parent;
-  /*while(next -> parent != NULL){
+  while(next -> parent != NULL){
     
-    if((tree ->lower_than(next -> parent -> pair -> key,tree -> current -> pair -> key))==0){
+    if((tree ->lower_than(tree -> current -> pair -> key,next -> parent -> pair -> key))==0){
       return next -> parent ->pair;
     }
     else next = next -> parent;
     
-  }*/
+  }
     return NULL;
 }
