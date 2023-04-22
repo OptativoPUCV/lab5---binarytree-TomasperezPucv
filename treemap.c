59cf11b399581492d3f885c19fe91e7287a05341
Replit user <>
2023-04-22 16:04:58 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -202,7 +202,7 @@ Pair * nextTreeMap(TreeMap * tree) {
   }
   //next = next -> parent;
   while(next -> parent != NULL){
-    if((tree ->lower_than(next -> parent -> pair -> key,next -> pair -> key ))==1){
+    if((tree ->lower_than(next -> parent -> pair -> key,tree -> current))==0){
       return next -> parent ->pair;
     }
     next = next -> parent;
